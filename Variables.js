72da133ecced8d62de7bc3ab26731e14cbc99ba8
CGRM2 <136513110+CGRM2@users.noreply.github.com>
2024-07-27 15:09:33 -0600

    Add files via upload

---
+++ Variables.js
@@ -0,0 +1,7 @@
+//Variables Globales 
+var Cristian_Gabriel_Ramirez_Morales = "Cristian Gabriel Ramirez Morales";
+
+//Variables Locales
+let Codigo
+
+document.write (Cristian_Gabriel_Ramirez_Morales);
